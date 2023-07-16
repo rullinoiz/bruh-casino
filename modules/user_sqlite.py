02108a3263df06e206ef1e49fa02d244e85f0af6
rullinoiz <46585763+rullinoiz@users.noreply.github.com>
2023-07-16 04:37:55 -0700

    user_instance class

--- modules/user_sqlite.py
+++ modules/user_sqlite.py
@@ -1,15 +1,20 @@
 import sqlite3 as sq
+
+import discord.ext.commands
 #from bot_config import bot_config
+from discord.abc import User
 from discord import Member
-from typing import Union,Any,Iterable
+from typing import Union, Any, Iterable, overload
+
+class user_instance: pass
 
 class user:
     s = sq.connect(r'user.db')
     c = s.cursor()
 
     @classmethod
-    def ensure_existence(cls, userid:Union[int, Member], add_new:bool=False) -> bool:
-        userid:int = userid if type(userid) != Member else userid.id
+    def ensure_existence(cls, userid:Union[int, User, user_instance], add_new:bool=False) -> bool:
+        userid:int = userid if type(userid) is int else userid.id
         if cls.c.execute('select id from user where id = ?;',(userid,)).fetchone() is None:
             if add_new:
                 user.c.execute('insert into user(id) values(?);',(userid,))
@@ -18,34 +23,34 @@ class user:
         return True
 
     @classmethod
-    def write(cls, userid:Union[int, Member], stat:str, content:Any) -> None:
-        userid:int = userid if type(userid) != Member else userid.id
+    def write(cls, userid:Union[int, User, user_instance], stat:str, content:Any) -> None:
+        userid:int = userid if type(userid) is int else userid.id
         t = cls.ensure_existence(userid, True)
         cls.c.execute(f'update user set {stat} = ? where id = ?;',(content,userid))
         cls.s.commit()
 
     @classmethod
-    def read(cls, userid:Union[int, Member], stat:str) -> int:
-        userid: int = userid if type(userid) != Member else userid.id
+    def read(cls, userid:Union[int, User, user_instance], stat:str) -> int:
+        userid: int = userid if type(userid) is int else userid.id
         t = cls.ensure_existence(userid, True)
         return cls.c.execute(f'select {stat} from user where id = ?;',(userid,)).fetchone()[0]
 
     @classmethod
-    def add(cls, userid:Union[int, Member], stat:Union[str, Iterable[str]], value:Union[int, Iterable[int]]) -> None:
-        userid:int = userid if type(userid) != Member else userid.id
+    def add(cls, userid:Union[int, User, user_instance], stat:Union[str, Iterable[str]], value:Union[int, Iterable[int]]) -> None:
+        userid:int = userid if type(userid) is int else userid.id
         t = cls.ensure_existence(userid, True)
         if type(stat) is str:
             cls.c.execute(f'update user set {stat} = {stat} {("+" if value > 0 else "-")} ? where id = ?;', (abs(value), userid))
             cls.s.commit()
         elif type(stat) is tuple:
-            if len(stat) != len(value): raise ValueError('stat and variable are of different length')
+            if len(stat) != len(value): raise ValueError('stat and value are of different length')
             command = f'update user set {", ".join(stat[x] + " = " + stat[x] + (" + " if value[x] > 0 else " - ") + str(value[x]) for x in range(0,len(stat)))} where id = {userid}'
             cls.c.execute(command)
             cls.s.commit()
 
     @classmethod
-    def subtract(cls, userid:Union[int, Member], stat:str, value:int) -> None:
-        userid: int = userid if type(userid) != Member else userid.id
+    def subtract(cls, userid:Union[int, Member, user_instance], stat:str, value:int) -> None:
+        userid: int = userid if type(userid) is int else userid.id
         t = cls.ensure_existence(userid, True)
         cls.c.execute(f'update user set {stat} = {stat} - ? where id = ?', (abs(value), userid))
         cls.s.commit()
@@ -62,3 +67,29 @@ class user:
                 return True
         
         return False
+
+class user_instance:
+    @overload
+    def __init__(self, user:Union[int, User]) -> None:
+        self.id: int = user if type(user) != User else user.id
+
+    def __init__(self, ctx:discord.ext.commands.Context) -> None:
+        self.id: int = ctx.author.id
+
+    def __getitem__(self, stat:str) -> int:
+        return user.read(self, stat)
+
+    def __setitem__(self, stat:str, value:str) -> None:
+        return user.write(self, stat, value)
+
+    def ensure_existence(self, add_new:bool=False) -> bool:
+        return user.ensure_existence(self, add_new)
+
+    def add(self, stat:Union[str, Iterable[str]], value:Union[int, Iterable[int]]) -> None:
+        return user.add(self, stat, value)
+
+    def read(self, stat:Union[str, Iterable[str]]) -> int:
+        return user.read(self, stat)
+
+    def subtract(self, stat:Union[str, Iterable[str]], value:Union[int, Iterable[int]]) -> None:
+        return user.subtract(self, stat, value)
